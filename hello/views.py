53696fb445c49deefb1fbc7761eae874e48fd43f
Rigdon Ware <rigdon.ware@gmail.com>
2017-04-01 18:08:06 +0000

    created hello project with and filled views.py to display hello, world with the time

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
+import time
 # Create your views here.
+
+def index(request):
+    return HttpResponse('hello, world.' + time.strftime('%c'))
